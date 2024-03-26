cf87280c9b61fc94c81a3f27661609db61f383a7
LiorLindhout <127389173+LiorLindhout@users.noreply.github.com>
2024-03-26 11:53:30 +0100

    Add files via upload

---
+++ Project/Gerecht.java
@@ -0,0 +1,26 @@
+package Project;
+
+import java.util.List;
+
+public class Gerecht {
+    private String naam;
+    private List<Ingredient> ingredienten;
+
+    public Gerecht(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void voegIngredientToe(String naam, int hoeveelheid, String eenheid) {
+        Ingredient nieuwIngredient = new Ingredient(naam, hoeveelheid, eenheid);
+        ingredienten.add(nieuwIngredient);
+    }
+
+}
---
+++ Project/Ontbijt.java
@@ -0,0 +1,17 @@
+package Project;
+
+import java.util.ArrayList;
+
+public class Ontbijt extends Eetmoment {
+    private ArrayList<Gerecht> gerechten;
+
+    @Override
+    public void voegGerechtToe(String gerechtNaam) {
+        Gerecht nieuwGerecht = new Gerecht(gerechtNaam);
+        gerechten.add(nieuwGerecht);
+    }
+
+    @Override
+    public void voegBereiderToe(String Bereider) {
+    }
+}
